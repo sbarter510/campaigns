a9ef31fe6e28c55793698a4bdc1fb12b02f27851
Stephen Barter <sbarter510@gmail.com>
2021-11-07 00:30:04 -0700

    jwt auth added

--- campaigns/src/components/Profile/Profile.js
+++ campaigns/src/components/Profile/Profile.js
@@ -1,8 +1,25 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { Context } from "../../context/context";
+import axios from "axios";
+
+axios.defaults.headers.common["Authorization"] =
+  "Bearer " + localStorage.getItem("token");
 
 export default function Profile() {
   const [state, dispatch] = useContext(Context);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/user/auth")
+      .then((res) => {
+        console.log(res);
+        return dispatch({
+          type: "LOG_IN",
+          payload: res.data.userName,
+        });
+      })
+      .catch((err) => console.log(err));
+  }, [dispatch]);
 
   //need to verify token and only display if valid
   return (
