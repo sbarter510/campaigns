f3c5aba703049a77042d2bf892c581520720a46c
Stephen Barter <sbarter510@gmail.com>
2021-11-21 16:29:53 -0800

    push before branch

--- campaigns/src/components/Profile/ProfileContent/ProfileContent.js
+++ campaigns/src/components/Profile/ProfileContent/ProfileContent.js
@@ -25,33 +25,40 @@ export default function ProfileContent() {
         </p>
         <hr />
       </div>
-      <div className="col s12 m4">
-        <h2 className="center">Information</h2>
-        <ul>
-          <li>Name:</li>
-          <li>Email:</li>
-          <li>Phone:</li>
-          <li>Address:</li>
-        </ul>
-      </div>
-      <div className="col s12 m8">
-        <h2 className="center">Content</h2>
-        <div class="carousel">
-          <a class="carousel-item" href="#one!">
-            <img src="https://lorempixel.com/250/250/nature/1" />
-          </a>
-          <a class="carousel-item" href="#two!">
-            <img src="https://lorempixel.com/250/250/nature/2" />
-          </a>
-          <a class="carousel-item" href="#three!">
-            <img src="https://lorempixel.com/250/250/nature/3" />
-          </a>
-          <a class="carousel-item" href="#four!">
-            <img src="https://lorempixel.com/250/250/nature/4" />
-          </a>
-          <a class="carousel-item" href="#five!">
-            <img src="https://lorempixel.com/250/250/nature/5" />
-          </a>
+      <div className="row" style={{ height: "500px" }}>
+        <div className="col s12 m4">
+          <h2>Information</h2>
+
+          <ul>
+            <li>Name:</li>
+            <li>Email:</li>
+            <li>Phone:</li>
+            <li>Address:</li>
+          </ul>
+        </div>
+
+        <div className="col s12 m8">
+          <div className="card">
+            <h2 className="center">Content</h2>
+
+            <div class="carousel">
+              <a class="carousel-item" href="#one!">
+                <img src="https://lorempixel.com/250/250/nature/1" />
+              </a>
+              <a class="carousel-item" href="#two!">
+                <img src="https://lorempixel.com/250/250/nature/2" />
+              </a>
+              <a class="carousel-item" href="#three!">
+                <img src="https://lorempixel.com/250/250/nature/3" />
+              </a>
+              <a class="carousel-item" href="#four!">
+                <img src="https://lorempixel.com/250/250/nature/4" />
+              </a>
+              <a class="carousel-item" href="#five!">
+                <img src="https://lorempixel.com/250/250/nature/5" />
+              </a>
+            </div>
+          </div>
         </div>
       </div>
     </div>
